b90c809d6a41aa6ccdd6819e1de0ebff36a02c81
Pasha Zubak <pashazubak@gmail.com>
2022-05-29 12:00:10 +0300

    styled main page

--- web/pages/404.js
+++ web/pages/404.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
-import Header from '~/scenes/sections/Header'
+import { Layout } from '~/components/Layout'
 // import { Page404 } from '~/scenes/ErrorPage'
 
-export default function Custom404() {
+export const Custom404 = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Error 404 - page not found</title>
       </Head>
-      <Header />
-      <main className="main-content">Error page</main>
-    </>
+      <main className="main-content">
+          <div className="container">
+              <h1 className="text-center">Error page</h1>
+          </div>
+      </main>
+    </Layout>
   )
 }
+
+export default Custom404
