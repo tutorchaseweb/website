b23a3e3d0e432281d461c6ebecc644fe7559d452
Pasha Zubak <pashazubak@gmail.com>
2022-07-08 01:03:39 +0300

    minor changes

---
+++ web/pages/access-programme.js
@@ -0,0 +1,16 @@
+import { Layout } from '~/components/Layout'
+
+export const AccessProgramme = () => {
+  return (
+    <Layout>
+      <section>
+        <div className="container">
+          <h1>Access Programme</h1>
+          <h3>TutorChase Access</h3>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default AccessProgramme
---
+++ web/pages/business-partnerships.js
@@ -0,0 +1,15 @@
+import { Layout } from '~/components/Layout'
+
+export const BusinessPartnerships = () => {
+  return (
+    <Layout>
+      <section>
+        <div className="container">
+          <h1>Business Partnerships</h1>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default BusinessPartnerships
