acf13e73ae033f24a822cdcfde749ca4211ea63b
Pasha Zubak <pashazubak@gmail.com>
2022-07-14 13:31:34 +0300

    create  reviews page

--- web/pages/reviews.js
+++ web/pages/reviews.js
@@ -1,23 +1,14 @@
+import Head from 'next/head'
 import { Layout } from '~/components/Layout'
+import { ReviewsPage } from '~/scenes/pages/Reviews'
 
 export const Reviews = () => {
   return (
     <Layout>
-      <section>
-        <div className="container">
-          <h1>Reviews</h1>
-          <div className="content">
-            <iframe
-              src="https://widget.reviews.co.uk/compound/widget?elementId=reviews-widget-summon-compound&amp;version=2&amp;&amp;store=admissionstutors&amp;primaryClr=%23f47e27&amp;neutralClr=%23333333&amp;reviewTextClr=%23333333&amp;layout=fullWidth&amp;numReviews=40&amp;version=13b&amp;contentMode=company%3Bthird-party"
-              frameBorder="0"
-              width="100%"
-              title="Reviews Compound Widget"
-              height="6530"
-              className="hide-me show-me"
-            />
-          </div>
-        </div>
-      </section>
+      <Head>
+        <title>Reviews</title>
+      </Head>
+      <ReviewsPage />
     </Layout>
   )
 }
