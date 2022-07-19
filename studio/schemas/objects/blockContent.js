9f1cbcc58478c351eeca03f0747ca771f0314857
Pasha Zubak <pashazubak@gmail.com>
2022-07-19 10:02:26 +0300

    minor changes

--- studio/schemas/objects/blockContent.js
+++ studio/schemas/objects/blockContent.js
@@ -35,6 +35,7 @@ export default {
         // preference or highlighting by editors.
         decorators: [
           { title: 'Strong', value: 'strong' },
+          { title: 'Code', value: 'code' },
           { title: 'Emphasis', value: 'em' },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
