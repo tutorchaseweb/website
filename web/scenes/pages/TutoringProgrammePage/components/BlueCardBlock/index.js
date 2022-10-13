4fc61ea72364a898fa2e02fc78f25ae6177c2a39
sofia-ost <sofia.ost4@gmail.com>
2022-10-13 22:01:59 +0300

    fix: replace logo for NTP section

--- web/scenes/pages/TutoringProgrammePage/components/BlueCardBlock/index.js
+++ web/scenes/pages/TutoringProgrammePage/components/BlueCardBlock/index.js
@@ -1,11 +1,10 @@
 import { PortableText } from '@portabletext/react'
-import logoNTP from '~/assets/images/logo_NTP.png'
 import styles from './style.module.scss'
 
-export const BlueCardBlock = ({ title = '', content = [], className = '' }) => {
+export const BlueCardBlock = ({ title = '', content = [], image = {}, className = '' }) => {
   return (
     <div className={`card mx-auto rounded-small flex ${styles.card} ${className}`}>
-      <img src={logoNTP.src} alt="photo of a woman" className={styles.card_logo} />
+      <img src={image?.src} alt="photo of a woman" className={styles.card_logo} />
       <h2 className="section-title fw-600 mb-2x w-full w-1/2_lg">{title}</h2>
       <div className="content w-full w-1/2_lg">
         <PortableText value={content} />
