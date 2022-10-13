ad185a688dabcd76d11cdb13c885ae31b24f0efb
sofia-ost <sofia.ost4@gmail.com>
2022-10-13 22:42:25 +0300

    fix: test deploy

--- web/scenes/pages/TutoringProgrammePage/components/BlueCardBlock/index.js
+++ web/scenes/pages/TutoringProgrammePage/components/BlueCardBlock/index.js
@@ -1,4 +1,5 @@
 import { PortableText } from '@portabletext/react'
+import Image from 'next/image'
 import logoNTP from '~/assets/images/logo_ntp.png'
 import styles from './style.module.scss'
 
@@ -6,6 +7,9 @@ export const BlueCardBlock = ({ title = '', content = [], className = '' }) => {
   return (
     <div className={`card mx-auto rounded-small flex ${styles.card} ${className}`}>
       <img src={logoNTP.src} alt="photo of a woman" className={styles.card_logo} />
+      <div className={styles.section_logo}>
+        <Image src={logoNTP.src} layout="fill" alt="NTP logo" />
+      </div>
       <h2 className="section-title fw-600 mb-2x w-full w-1/2_lg">{title}</h2>
       <div className="content w-full w-1/2_lg">
         <PortableText value={content} />
