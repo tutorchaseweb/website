127d16d21bdd873d30ce9c74f89a38a55c2fe731
sofia-ost <88640668+sofia-ost@users.noreply.github.com>
2022-12-22 14:10:28 +0200

    fix: check next sitemap file

--- web/next-sitemap.config.js
+++ web/next-sitemap.config.js
@@ -2,7 +2,7 @@ const siteUrl = process.env.NEXT_PUBLIC_BASE_URL
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
-  exclude: ['/sitemap.xml', '/server-sitemap.xml', '/404', '/tutor-submission', '/form-submission'],
+  exclude: ['/sitemap', '/404', '/tutor-submission', '/form-submission'],
   robotsTxtOptions: {
     policies: [
       {
